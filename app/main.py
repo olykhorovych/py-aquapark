cca6894b68bb8ae63a99de219a8b1e26582b9419
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-03 22:33:15 +0200

    Solution6

--- app/main.py
+++ app/main.py
@@ -69,7 +69,11 @@ class AdultSlideLimitationValidator(SlideLimitationValidator):
 
 
 class Slide:
-    def __init__(self, name: str, limitation_class: SlideLimitationValidator) -> None:
+    def __init__(
+        self,
+        name: str,
+        limitation_class: SlideLimitationValidator
+    ) -> None:
         self.name = name
         self.limitation_class = limitation_class
 
